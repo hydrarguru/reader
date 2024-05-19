c25f31e3356d66dda7b9c5fb14a74c6f22c1aab4
hydrarguru <henrik@engqvist.org>
2024-05-19 17:18:13 +0200

    chore: Add Sequelize configuration for SQLite database

--- backend/index.ts
+++ backend/index.ts
@@ -1,11 +1,21 @@
 import express from 'express';
 import cors from 'cors';
+import { Sequelize } from 'sequelize';
 
 const app = express();
 const port = 8080;
 app.use(cors);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+const sequelize = new Sequelize({
+    dialect: 'sqlite',
+    storage: 'db.sqlite'
+});
+
+sequelize.query('PRAGMA foreign_keys = ON');
+
+
 
 
 app.listen(port, () => {
