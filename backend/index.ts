dd22b6990269d9451a0887a5c8f9d260ba8b0d19
hydrarguru <henrik@engqvist.org>
2024-05-24 23:22:25 +0200

    feat(backend): initialize database.

--- backend/index.ts
+++ backend/index.ts
@@ -1,21 +1,30 @@
 import express from 'express';
-import cors from 'cors';
 import { Sequelize } from 'sequelize';
+import init_db  from './util/init_db';
+//import cors from 'cors';
+import fs from 'fs';
 
 const app = express();
 const port = 8080;
-app.use(cors);
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
 const sequelize = new Sequelize({
     dialect: 'sqlite',
     storage: 'db.sqlite'
 });
 
-sequelize.query('PRAGMA foreign_keys = ON');
+//app.use(cors);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 
+init_db(sequelize);
 
 
 app.listen(port, () => {
---
+++ backend/util/init_db.ts
@@ -0,0 +1,87 @@
+import { create } from 'domain';
+import { Sequelize, DataTypes } from 'sequelize';
+
+
+export default function init_db(seq: Sequelize) {
+    seq.query('PRAGMA foreign_keys = ON');
+
+    seq.define('users', {
+        user_id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: false,
+            allowNull: false
+        },
+        user_name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        user_email: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        user_password: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        user_posts: {
+            type: DataTypes.ARRAY(DataTypes.UUIDV4),
+            allowNull: true,
+            references: {
+                model: 'posts',
+                key: 'post_id'
+            }
+        },
+    })
+
+    seq.define('posts', {
+        post_id: {
+            type: DataTypes.UUIDV4,
+            primaryKey: true,
+            autoIncrement: false,
+            allowNull: false
+        },
+        post_title: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        post_content: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        post_author: {
+            type: DataTypes.UUIDV4,
+            allowNull: false,
+            references: {
+                model: 'users',
+                key: 'user_id'
+            }
+        },
+    })
+    seq.define('community', {
+        community_id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: false,
+            allowNull: false
+        },
+        community_name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        community_description: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        communty_posts: {
+            type: DataTypes.ARRAY(DataTypes.UUIDV4),
+            allowNull: true,
+            references: {
+                model: 'posts',
+                key: 'post_id'
+            }
+        },
+    })
+
+    seq.sync({ force: true });
+}
