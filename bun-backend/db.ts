fbe6c79806f27f00d22dad92fc33d0d7ddddb143
hydrarguru <henrik@engqvist.org>
2024-05-30 04:16:40 +0200

    feat(db): getAll & getOne functions.

--- bun-backend/db.ts
+++ bun-backend/db.ts
@@ -1,4 +1,4 @@
-import { Sequelize } from "sequelize";
+import { Sequelize, QueryTypes } from "sequelize";
 import { usersTable } from "./models/User";
 import { postsTable } from "./models/Post";
 import { communitiesTable } from "./models/Community";
@@ -18,7 +18,8 @@ export const Client = new Sequelize({
     host: process.env.DB_HOST,
     database: process.env.DB_NAME,
     username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD
+    password: process.env.DB_PASSWORD,
+    logging: console.log
 });
 
 export async function createTables() {
@@ -28,3 +29,23 @@ export async function createTables() {
     await addForeignKey("Posts", "post_author", "Users", "user_id");
     await addForeignKey("Posts", "community_id", "Communities", "community_id");
 };
+
+export async function getAll(table: string) {
+    const [results, metadata] = await Client.query(`SELECT * FROM ${table}`);
+    console.log(metadata);
+    return results;
+}
+
+export async function getOne(table: string, column: string, value: string | number) : Promise<object | null> {
+    const results= await Client.query(`SELECT * FROM ${table} WHERE ${column} = :value`, {
+        type: QueryTypes.SELECT,
+        replacements: { value }
+    });
+    if (results.length === 0) {
+        return null;
+    }
+    else {
+        return results;
+    }
+}
+
