917dac08ccb2f20376f219467cea99968489cbe8
hydrarguru <henrik@engqvist.org>
2024-05-30 06:04:43 +0200

    feat(types): added types for Community, Post & User.

---
+++ bun-backend/types/CommunityType.ts
@@ -0,0 +1,8 @@
+export type CommunityType = {
+    community_id: string;
+    community_name: string;
+    community_image_url: string;
+    community_desc: string;
+    created_at: Date;
+    modified_at: Date;
+};
---
+++ bun-backend/types/UserType.ts
@@ -0,0 +1,8 @@
+export type UserType = {
+    user_id: string;
+    username: string;
+    email: string;
+    password: string;
+    created_at: Date;
+    modified_at: Date;
+};
