6202b43cc024e97397e457e2b70d59ef137ece62
hydrarguru <henrik@engqvist.org>
2024-06-04 20:06:47 +0200

    feat: routing.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,13 +2,26 @@ import 'rsuite/dist/rsuite.min.css';
 import { CustomProvider } from 'rsuite';
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App.tsx'
+import CommunityPage from './pages/CommunityPage.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />
+  },
+  {
+    path: '/c/:category',
+    element: <CommunityPage />
+  },
+])
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <CustomProvider theme='dark'>
-      <App />
+      <RouterProvider router={router}/>
     </CustomProvider>
   </React.StrictMode>
 )
