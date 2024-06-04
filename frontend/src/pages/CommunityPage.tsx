2dc588bf6ed2afdf8470708d0b2d9433df94b427
hydrarguru <henrik@engqvist.org>
2024-06-04 20:49:27 +0200

    refactor(pages): regex filter for '/c/'.

--- frontend/src/pages/CommunityPage.tsx
+++ frontend/src/pages/CommunityPage.tsx
@@ -1,8 +1,13 @@
+import { useLocation } from "react-router-dom";
+
 
 const CommunityPage = () => {
+    const communityName = useLocation().pathname.replace(/\/c\//, '');
+
     return (
         <div>
             <h1>Community Page</h1>
+            <p>Community Name: {communityName}</p>
         </div>
     );
 };
