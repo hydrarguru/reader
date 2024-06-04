dc065885d1f8e5ce6339b4a9736980ace3958855
hydrarguru <henrik@engqvist.org>
2024-06-05 00:59:27 +0200

    fix(backend): allow cross origin

--- bun-backend/index.ts
+++ bun-backend/index.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import { generateTables } from "./db";
 import { UserRouter } from "./routes/User"
 import { PostRouter } from "./routes/Post";
@@ -6,9 +7,11 @@ import { CommunityRouter } from "./routes/Community";
 
 const shouldCreateTables = false;
 const port = 8080;
+const corsConfig = cors({ origin: "*" });
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(corsConfig)
 
 app.use(UserRouter);
 app.use(CommunityRouter);
