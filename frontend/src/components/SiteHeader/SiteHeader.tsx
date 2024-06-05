93156fd5c85668b0edec113ea07a3488ece96be7
hydrarguru <henrik@engqvist.org>
2024-06-05 07:56:43 +0200

    refactor(frontend): login/register links added to header.

--- frontend/src/components/SiteHeader/SiteHeader.tsx
+++ frontend/src/components/SiteHeader/SiteHeader.tsx
@@ -1,6 +1,7 @@
 import { Navbar, Nav } from 'rsuite';
 import PeoplesCostomizeIcon from '@rsuite/icons/PeoplesCostomize';
 import OperatePeopleIcon from '@rsuite/icons/OperatePeople';
+import { Link } from 'react-router-dom';
 
 
 function SiteHeader() {
@@ -8,10 +9,8 @@ function SiteHeader() {
     <Navbar>
     <Navbar.Brand href="/">Not Reddit</Navbar.Brand>
     <Nav pullRight>
-      <Nav.Menu title="Login / Signup">
-        <Nav.Item icon={<PeoplesCostomizeIcon />}>Login</Nav.Item>
-        <Nav.Item icon={<OperatePeopleIcon />}>Signup</Nav.Item>
-      </Nav.Menu>
+        <Nav.Item icon={<PeoplesCostomizeIcon />}><Link to='/login'>Login</Link></Nav.Item>
+        <Nav.Item icon={<OperatePeopleIcon />}><Link to='/register'>Register</Link></Nav.Item>
     </Nav>
   </Navbar>
   )
