f823bb3803adf779df9b6788252ba7957761825d
hydrarguru <henrik@engqvist.org>
2024-06-09 23:50:56 +0200

    refactor(RegisterForm): removed unused imports.

--- frontend/src/components/Forms/RegisterForm.tsx
+++ frontend/src/components/Forms/RegisterForm.tsx
@@ -1,42 +1,28 @@
 import { Form, ButtonToolbar, Button } from 'rsuite';
-import React from 'react';
-import { useFormik } from 'formik';
 
 const RegisterForm = () => {
-    const formik = useFormik({
-        initialValues: {
-            username: '',
-            email: '',
-            password: '',
-            passwordConfirm: ''
-        },
-        onSubmit: values => {
-            alert(JSON.stringify(values, null, 2));
-        },
-    });
-
     return (
-    <Form fluid onSubmit={formik.handleSubmit}>
+    <Form fluid>
       <Form.Group controlId="username">
         <Form.ControlLabel>Username</Form.ControlLabel>
-        <Form.Control id='username' name="username" autoComplete="off" onChange={formik.handleChange}/>
+        <Form.Control id='username' name="username" autoComplete="off"/>
         <Form.HelpText>Required</Form.HelpText>
       </Form.Group>
 
       <Form.Group controlId="email">
         <Form.ControlLabel>Email</Form.ControlLabel>
-        <Form.Control id='email' name="email" type="email" autoComplete="off" onChange={formik.handleChange}/>
+        <Form.Control id='email' name="email" type="email" autoComplete="off"/>
         <Form.HelpText>Required</Form.HelpText>
       </Form.Group>
 
       <Form.Group controlId="password">
         <Form.ControlLabel>Password</Form.ControlLabel>
-        <Form.Control id='password' name="password" type="password" autoComplete="off" onChange={formik.handleChange}/>
+        <Form.Control id='password' name="password" type="password" autoComplete="off"/>
       </Form.Group>
 
       <Form.Group controlId="password-confirm">
         <Form.ControlLabel>Confirm Password</Form.ControlLabel>
-        <Form.Control id='password-confirm' name="password-confirm" type="password" autoComplete="off" onChange={formik.handleChange}/>
+        <Form.Control id='password-confirm' name="password-confirm" type="password" autoComplete="off"/>
       </Form.Group>
 
       <Form.Group>
