90f3a7de4424860396d8ce34329c7984eaaa0f8d
hydrarguru <henrik@engqvist.org>
2024-06-09 04:45:53 +0200

    refactor(header): removed Link component.

--- frontend/src/components/SiteHeader/SiteHeader.tsx
+++ frontend/src/components/SiteHeader/SiteHeader.tsx
@@ -1,7 +1,6 @@
 import { Navbar, Nav } from 'rsuite';
 import PeoplesCostomizeIcon from '@rsuite/icons/PeoplesCostomize';
 import OperatePeopleIcon from '@rsuite/icons/OperatePeople';
-import { Link } from 'react-router-dom';
 
 
 function SiteHeader() {
@@ -9,8 +8,8 @@ function SiteHeader() {
     <Navbar>
     <Navbar.Brand href="/">Not Reddit</Navbar.Brand>
     <Nav pullRight>
-        <Nav.Item icon={<PeoplesCostomizeIcon />}><Link to='/login'>Login</Link></Nav.Item>
-        <Nav.Item icon={<OperatePeopleIcon />}><Link to='/register'>Register</Link></Nav.Item>
+        <Nav.Item icon={<PeoplesCostomizeIcon />} href='/login'>Login</Nav.Item>
+        <Nav.Item icon={<OperatePeopleIcon />} href='/register'>Register</Nav.Item>
     </Nav>
   </Navbar>
   )
