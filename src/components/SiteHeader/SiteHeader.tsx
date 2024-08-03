adb4b2da9020d0d3fdee74312a9f749748265bb0
hydrarguru <henrik@engqvist.org>
2024-08-04 01:02:03 +0200

    refactor(SiteHeader): changed title.

--- src/components/SiteHeader/SiteHeader.tsx
+++ src/components/SiteHeader/SiteHeader.tsx
@@ -6,7 +6,7 @@ import OperatePeopleIcon from '@rsuite/icons/OperatePeople';
 function SiteHeader() {
   return (
     <Navbar>
-    <Navbar.Brand href="/">Not Reddit</Navbar.Brand>
+    <Navbar.Brand href="/">Reader</Navbar.Brand>
     <Nav pullRight>
         <Nav.Item icon={<PeoplesCostomizeIcon />} href='/login'>Login</Nav.Item>
         <Nav.Item icon={<OperatePeopleIcon />} href='/register'>Register</Nav.Item>
