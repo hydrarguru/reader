7ddaa0c3e69ed285c8e5c5eed802b45c3a9a9936
hydrarguru <henrik@engqvist.org>
2024-09-25 11:33:38 +0200

    light/dark theme toggle.

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,14 @@ import './index.css';
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import { ThemeProvider } from './components/ThemeProvider.tsx';
+
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider defaultTheme='light' storageKey='vite-ui-theme'>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 )
