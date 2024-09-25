071ca60911da7c441c934e1f93847312d6ea2840
hydrarguru <henrik@engqvist.org>
2024-09-25 12:48:52 +0200

    added theme toggle button component.

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import './index.css';
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import { ThemeProvider } from './components/ThemeProvider.tsx';
+import { ThemeProvider } from './components/Theme/ThemeProvider.tsx';
 
 
 
