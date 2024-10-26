cb5eb5809874e3a105c7f68c4b22314741970666
hydrarguru <henrik@engqvist.org>
2024-10-26 19:23:57 +0200

    removed unused nav items.

--- src/components/SiteHeader/SiteHeader.tsx
+++ src/components/SiteHeader/SiteHeader.tsx
@@ -1,16 +1,10 @@
-import { Navbar, Nav } from 'rsuite';
-import PeoplesCostomizeIcon from '@rsuite/icons/PeoplesCostomize';
-import OperatePeopleIcon from '@rsuite/icons/OperatePeople';
+import { Navbar } from 'rsuite';
 
 
 function SiteHeader() {
   return (
     <Navbar>
     <Navbar.Brand href="/">Reader</Navbar.Brand>
-    <Nav pullRight>
-        <Nav.Item icon={<PeoplesCostomizeIcon />} href='#'>Login</Nav.Item>
-        <Nav.Item icon={<OperatePeopleIcon />} href='#'>Register</Nav.Item>
-    </Nav>
   </Navbar>
   )
 }
