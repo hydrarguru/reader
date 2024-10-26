1418b0c235adbbcab1bc335885fbe31c2a246a64
hydrarguru <henrik@engqvist.org>
2024-10-26 22:15:49 +0200

    updated App.tsx.

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,10 @@
 import { useEffect, useState } from 'react';
 import type { Community } from './types/CommunityType';
-import type { Post } from './types/PostType';
 import { NavBar } from './components/Navigation/NavBar';
-
-//import SiteHeader from './components/SiteHeader/SiteHeader';
-//import CommunityPost from './components/Posts/Post';
-//import { CommunityHeaderInfo, CommunityHeaderNoInfo } from './components/CommunityInformation';
-//import CreatePost from './components/Modals/CreatePost';
-//import CreateCommunity from './components/Modals/CreateCommunity';
+import { CommunityContainer } from './components/Community/CommunityContainer';
 
 async function getCommunities() {
-  const result = await fetch(`${import.meta.env.VITE_READER_BACKEND_URL}/community`)
+  const result = await fetch(`${import.meta.env.VITE_BACKEND_URL}/communities`)
     .then((res) => res.json())
     .catch((err) => console.error(err));
   if (result === undefined || result === null) {
@@ -20,68 +14,47 @@ async function getCommunities() {
   }
 }
 
-async function getCommunityPosts(community_id: string) {
-  const data = await fetch(`${import.meta.env.VITE_READER_BACKEND_URL}/c/${community_id}/posts`)
-    .then((res) => res.json())
-    .catch((err) => console.error(err));
-  if (data === undefined || data === null) {
-    return;
-  } else {
-    return data as Post[];
-  }
-}
-
-async function getPosts() {
-  const data = await fetch(`${import.meta.env.VITE_READER_BACKEND_URL}/posts`)
-    .then((res) => res.json())
-    .catch((err) => console.error(err));
-  if (data === undefined || data === null) {
-    return;
-  } else {
-    return data as Post[];
-  }
-}
-
-const testCommunities = ['test1', 'test2', 'test3', 'test4', 'test5', 'test6', 'test7', 'test8', 'test9', 'test10'];
-
 function App() {
   const [communities, setCommunities] = useState<Community[] | null>(null);
   const [activeCommunity, setActiveCommunity] = useState<Community | null>(null);
-  const [communityPosts, setCommunityPosts] = useState<Post[] | null>(null);
-  const [allPosts, setAllPosts] = useState<Post[] | null>(null);
+
+  function handleCommunityChange(community: Community | null) {
+    setActiveCommunity(community);
+  }
 
   useEffect(() => {
     if (communities === null) {
       getCommunities().then((communities) => {
         if (communities !== undefined) {
+          console.log('Storing communities in session storage');
           setCommunities(communities);
         }
       });
     }
   }, [communities]);
 
-  useEffect(() => {
-    getPosts().then((posts) => {
-      if (posts !== undefined) {
-        setAllPosts(posts);
-      }
-    });
-  }, [allPosts]);
-
-  useEffect(() => {
-    if (activeCommunity !== null) {
-      getCommunityPosts(activeCommunity.community_id).then((posts) => {
-        if (posts !== undefined) {
-          setCommunityPosts(posts);
-        }
-      });
-    }
-  }, [activeCommunity]);
-
   return (
-    <div>
-      <NavBar listOfCommunities={testCommunities} isUserLoggedIn={true}/>
-    </div>
+    <main className='m-4'>
+      <NavBar communities={communities as Community[]} isUserLoggedIn={true} onCommunityChange={handleCommunityChange} />
+      {activeCommunity !== null ? (
+        <CommunityContainer community={activeCommunity} />
+      ) : (
+        <div className='my-4'>
+          {communities !== null ? (
+            <div className='grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
+              {communities.map((community) => (
+                <div key={community.community_id} className='bg-zinc-800 p-4 rounded-lg hover:bg-zinc-900 hover:cursor-pointer' onClick={() => handleCommunityChange(community)}>
+                  <h2 className='text-xl font-semibold'>{community.community_name}</h2>
+                  <p className='text-gray-200'>{community.community_desc}</p>
+                </div>
+              ))}
+            </div>
+          ) : (
+            <p>Loading...</p>
+          )}
+        </div>
+      )}
+    </main>
   );
 }
 
