9613d87cb35f10cc58a488f89beef64b481cf5c4
hydrarguru <henrik@engqvist.org>
2024-11-06 17:32:43 +0100

    styling tweaks
    fixed weird behavior where focused input fields would have blue outline.
    changed color of post button to green.
    added red cancel button that closes the modal.

--- src/components/Modals/CreateCommunityPostModal.tsx
+++ src/components/Modals/CreateCommunityPostModal.tsx
@@ -16,24 +16,29 @@ export function CreateCommunityPostModal(CreateCommunityPostModalProps: CreateCo
                 className='fixed inset-0 bg-black opacity-50'
                 onClick={CreateCommunityPostModalProps.modalClose}
             ></div>
-            <div className='bg-zinc-900 rounded-lg overflow-hidden shadow-xl transform transition-all sm:max-w-lg sm:w-full'>
+            <div className='bg-zinc-900 rounded-md overflow-hidden shadow-xl transform transition-all sm:max-w-lg sm:w-full'>
                 <div className='px-4 py-5 sm:p-6'>
                     <h3 className='text-lg leading-6 font-medium text-white'>
-                        Create a Post
+                        Creating a post in {CreateCommunityPostModalProps.communityName}
                     </h3>
                     <div className='mt-2'>
                         <input
                             type='text'
                             placeholder='Title'
-                            className='w-full p-2 mt-2 bg-zinc-800 text-white rounded-lg'
+                            className='w-full p-2 mt-2 bg-zinc-800 text-white rounded-md focus:outline-none'
                         />
                         <textarea
                             placeholder='Content'
-                            className='w-full p-2 mt-2 bg-zinc-800 text-white rounded-lg'
+                            className='w-full p-2 mt-2 bg-zinc-800 text-white rounded-md focus:outline-none'
                         ></textarea>
-                        <button className='p-2 mt-2 bg-zinc-800 text-white rounded-lg hover:bg-zinc-700'>
-                            Post
-                        </button>
+                        <div className="flex justify-between">
+                            <button className='p-2 mt-2 bg-green-700 text-white rounded-md hover:bg-green-800'>
+                                Post
+                            </button>
+                            <button className='p-2 mt-2 bg-red-700 text-white rounded-md hover:bg-red-800' onClick={CreateCommunityPostModalProps.modalClose}>
+                                Cancel
+                            </button>
+                        </div>
                     </div>
                 </div>
             </div>
