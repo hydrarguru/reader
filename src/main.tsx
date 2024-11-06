2313d540c7fef094ac1769b56dc00beefb704829
hydrarguru <henrik@engqvist.org>
2024-11-06 14:27:22 +0100

    removed routing

--- src/main.tsx
+++ src/main.tsx
@@ -2,34 +2,14 @@ import './assets/main.css';
 import { CustomProvider } from 'rsuite';
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import App from './App.tsx'
-import RegisterPage from './pages/RegisterPage.tsx';
-import LoginPage from './pages/LoginPage.tsx';
+import App  from './App.tsx'
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-  },
-  {
-    path: '/c/:community',
-    element: <App />
-  },
-  {
-    path: '/register',
-    element: <RegisterPage />
-  },
-  {
-    path: '/login',
-    element: <LoginPage />
-  }
-])
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <CustomProvider theme='dark'>
-      <RouterProvider router={router}/>
+        <App />
     </CustomProvider>
   </React.StrictMode>
 )
