fd402dbe37fb6c97854ca9c1d9e4052916b625cb
hydrarguru <henrik@engqvist.org>
2024-11-06 17:17:47 +0100

    Create post button now opens a create post modal

--- src/components/Community/CommunityHeader.tsx
+++ src/components/Community/CommunityHeader.tsx
@@ -1,5 +1,6 @@
-import { useState } from 'react';
+import { useState, useCallback } from 'react';
 import { Community } from '@/types/CommunityType';
+import { CreateCommunityPostModal } from '../Modals/CreateCommunityPostModal';
 
 const img_url =
   'https://images.unsplash.com/photo-1682692290240-ac21cb19b0eb?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D';
@@ -10,6 +11,11 @@ interface CommunityHeaderProps {
 
 export function CommunityHeader(CommunityHeaderProps: CommunityHeaderProps) {
   const [activeCommunity] = useState<Community | null>(CommunityHeaderProps.community);
+  const [modalState, setModalState] = useState<boolean>(false);
+
+  const handleModalState = useCallback(() => {
+    setModalState((prev) => !prev);
+  }, []);
 
   return (
     <div className='my-4'>
@@ -23,11 +29,12 @@ export function CommunityHeader(CommunityHeaderProps: CommunityHeaderProps) {
           {activeCommunity?.community_name || 'Community Name'}
         </h1>
         <div className='grid gap-2 grid-flow-col'>
-          <button className='px-4 py-2 rounded-full border border-zinc-400 hover:border-zinc-100'>
+          <button className='px-4 py-2 rounded-full border border-zinc-400 hover:border-zinc-100' onClick={handleModalState}>
             Create Post
           </button>
         </div>
       </div>
+      <CreateCommunityPostModal communityId={activeCommunity?.community_id as string} communityName={activeCommunity?.community_name as string} isOpen={modalState} modalClose={handleModalState} />
     </div>
   );
 }
