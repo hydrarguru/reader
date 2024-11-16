633871406fc4603eee3d8d13e2ced3d60cff90dd
hydrarguru <henrik@engqvist.org>
2024-11-16 19:50:37 +0100

    initial form styling.

--- src/components/Modals/CreateCommunityPostModal.tsx
+++ src/components/Modals/CreateCommunityPostModal.tsx
@@ -1,47 +1,129 @@
+import { useState } from 'react';
+import { createPost } from '@/api/posts';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+
+import { useToast } from '@/hooks/use-toast';
+import { Button } from '@/components/ui/button';
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { Textarea } from '../ui/textarea';
+
 interface CreateCommunityPostModalProps {
-    communityId: string;
-    communityName: string;
-    isOpen: boolean;
-    modalClose: () => void;
+  communityId: string;
+  communityName: string;
+  isOpen: boolean;
+  modalClose: () => void;
 }
 
+const createPostFormSchema = z.object({
+  community_id: z.string(),
+  post_author: z.string(),
+  post_title: z
+    .string()
+    .min(6, {
+      message: 'Post title must be at least 6 characters.',
+    })
+    .max(255),
+  post_content: z.string().min(6, {
+    message: 'Post content must be at least 6 characters.',
+  }),
+});
+
 export function CreateCommunityPostModal(CreateCommunityPostModalProps: CreateCommunityPostModalProps) {
-    return (
-        <div
-            className={`fixed inset-0 z-50 flex items-center justify-center ${
-                CreateCommunityPostModalProps.isOpen ? 'block' : 'hidden'
-            }`}
-        >
-            <div
-                className='fixed inset-0 bg-black opacity-50'
-                onClick={CreateCommunityPostModalProps.modalClose}
-            ></div>
-            <div className='bg-zinc-900 rounded-md overflow-hidden shadow-xl transform transition-all sm:max-w-lg sm:w-full'>
-                <div className='px-4 py-5 sm:p-6'>
-                    <h3 className='text-lg leading-6 font-medium text-white'>
-                        Creating a post in {CreateCommunityPostModalProps.communityName}
-                    </h3>
-                    <div className='mt-2'>
-                        <input
-                            type='text'
-                            placeholder='Title'
-                            className='w-full p-2 mt-2 bg-zinc-800 text-white rounded-md focus:outline-none'
-                        />
-                        <textarea
-                            placeholder='Content'
-                            className='w-full p-2 mt-2 bg-zinc-800 text-white rounded-md focus:outline-none'
-                        ></textarea>
-                        <div className="flex justify-between">
-                            <button className='p-2 mt-2 bg-green-700 text-white rounded-md hover:bg-green-800'>
-                                Post
-                            </button>
-                            <button className='p-2 mt-2 bg-red-700 text-white rounded-md hover:bg-red-800' onClick={CreateCommunityPostModalProps.modalClose}>
-                                Cancel
-                            </button>
-                        </div>
-                    </div>
-                </div>
-            </div>
+  const { toast } = useToast();
+  const form = useForm<z.infer<typeof createPostFormSchema>>({
+    resolver: zodResolver(createPostFormSchema),
+    defaultValues: {
+      community_id: CreateCommunityPostModalProps.communityId,
+      post_author: 'test_username',
+      post_title: '',
+      post_content: '',
+    },
+  });
+
+  const onCancel = () => {
+    form.reset();
+    CreateCommunityPostModalProps.modalClose();
+  }
+
+  const onSubmit = (data: z.infer<typeof createPostFormSchema>) => {
+    const error = false;
+
+    if (error) {
+      return toast({
+        type: 'background',
+        variant: 'destructive',
+        title: 'Error creating post',
+        description: 'An error occurred while creating the post.',
+        duration: 2000,
+      });
+    }
+      else {
+        toast({
+          type: 'background',
+          variant: 'default',
+          title: 'Created post',
+          description: 'Successfully created a post.',
+          duration: 1750,
+        });
+        console.dir(data);
+      }
+  }
+
+  return (
+    <div
+      className={`fixed inset-0 z-50 flex items-center justify-center ${
+        CreateCommunityPostModalProps.isOpen ? 'block' : 'hidden'
+      }`}
+    >
+      <div className='fixed inset-0 bg-black opacity-50' onClick={onCancel}></div>
+      <div className='bg-zinc-900 rounded-md overflow-hidden shadow-xl transform transition-all sm:max-w-lg sm:w-full'>
+        <div className='px-4 py-5 sm:p-6'>
+          <h3 className='text-lg leading-6 font-medium text-white'>
+            Creating a post in {CreateCommunityPostModalProps.communityName}
+          </h3>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className='mt-2'>
+              <FormField
+                control={form.control}
+                name='post_title'
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Post title:</FormLabel>
+                    <FormControl>
+                      <Input className='focus-visible:ring-0' placeholder='Post title...' autoComplete='false' {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name='post_content'
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Post content:</FormLabel>
+                    <FormControl>
+                      <Textarea className='focus-visible:ring-0' cols={5} placeholder='Post content...' autoComplete='false' {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <div className='flex justify-between'>
+                <Button className='mt-2' type='submit'>
+                  Submit
+                </Button>
+                <Button className='mt-2' variant="destructive" onClick={onCancel}>
+                  Cancel
+                </Button>
+              </div>
+            </form>
+          </Form>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import React, { Suspense } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { ThemeProvider } from './components/Theme/ThemeProvider.tsx';
-
+import { Toaster } from "@/components/ui/toaster"
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
@@ -11,6 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <ThemeProvider defaultTheme='dark' storageKey='vite-ui-theme'>
       <Suspense fallback={<div>Loading...</div>}>
       <App />
+      <Toaster />
       </Suspense>
     </ThemeProvider>
   </React.StrictMode>
