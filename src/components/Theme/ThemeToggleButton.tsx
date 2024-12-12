0df986376c7a61796a00f3681ab2a98971ccc919
hydrarguru <henrik@engqvist.org>
2024-12-12 21:50:29 +0100

    refactor: added props.

--- src/components/Theme/ThemeToggleButton.tsx
+++ src/components/Theme/ThemeToggleButton.tsx
@@ -3,7 +3,11 @@ import { Moon, Sun, SunMoon } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 import { useTheme } from '@/components/Theme/ThemeProvider';
 
-export function ThemeToggleButton() {
+interface ThemeToggleButtonProps {
+  className?: string;
+}
+
+export function ThemeToggleButton({ className }: ThemeToggleButtonProps) {
   const { setTheme } = useTheme();
   const [currentTheme, setCurrentTheme] = useState('light');
 
@@ -14,7 +18,7 @@ export function ThemeToggleButton() {
   }
 
   return (
-    <Button variant='outline' size='icon' onClick={() => handleThemeChange()}>
+    <Button className={className} variant='outline' size='icon' onClick={() => handleThemeChange()}>
       <Sun className='text-violet-600 dark:text-violet-600 hover:text-violet-800 dark:hover:text-violet-800 h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0' />
       <SunMoon className='text-violet-600 dark:text-violet-600 hover:text-violet-800 dark:hover:text-violet-800 absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100' />
       <span className='sr-only'>Toggle theme</span>
