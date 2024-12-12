0d0d3c81f8cea209981cd5862d7525f3e5fac6df
hydrarguru <henrik@engqvist.org>
2024-12-12 14:01:12 +0100

    feat: added tooltip on date hover to provide more info about the posted date.

--- src/components/Community/CommunityPost.tsx
+++ src/components/Community/CommunityPost.tsx
@@ -1,31 +1,9 @@
-import { ButtonGroup } from "./ButtonGroup";
-import { Skeleton } from "../ui/skeleton";
-import { Post } from "../../types/PostType";
+import { ButtonGroup } from './ButtonGroup';
+import { Skeleton } from '../ui/skeleton';
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/components/ui/tooltip';
+import { Post } from '../../types/PostType';
 
-
-export function CommunityPostSkeleton() {
-    return (
-      <div className='border text-gray-200 p-4 rounded-md mx-auto mb-4'>
-        <div className='flex items-center space-x-2 mb-2'>
-          <div className='flex items-center space-x-1'>
-            <Skeleton className='h-4 w-20' />
-          </div>
-        </div>
-  
-        {/* Post Title */}
-        <div className='text-lg mb-4 font-semibold text-gray-100'>
-          <Skeleton className='h-6 w-72' />
-        </div>
-  
-        {/* Interaction Bar */}
-        <div className='flex items-center space-x-6 text-sm text-gray-400'>
-          <div className='flex items-center space-x-1'>
-            <Skeleton className='h-4 w-20' />
-          </div>
-        </div>
-      </div>
-    );
-  }
+import { useState } from 'react';
 
 function getTimeBetweenDates(date1: Date, date2: Date): string {
   const diff = Math.abs(date1.getTime() - date2.getTime());
@@ -44,7 +22,50 @@ function getTimeBetweenDates(date1: Date, date2: Date): string {
   } else {
     return `${days} days ago`;
   }
+}
 
+export function CommunityPostDate({ createdAt }: { createdAt?: Date }) {
+  const [postDate] = useState<Date>(createdAt ? createdAt : new Date());
+  const dateToolTip = `Posted on ${postDate.toDateString() + ' at ' + postDate.toLocaleTimeString()}`;
+
+  return (
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <span className='hover:text-gray-500 cursor-default text-sm text-gray-400'>
+            {postDate ? getTimeBetweenDates(postDate, new Date()) : 'Unknown date'}
+          </span>
+        </TooltipTrigger>
+        <TooltipContent>
+          <p>{dateToolTip}</p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  );
+}
+
+export function CommunityPostSkeleton() {
+  return (
+    <div className='border text-gray-200 p-4 rounded-md mx-auto mb-4'>
+      <div className='flex items-center space-x-2 mb-2'>
+        <div className='flex items-center space-x-1'>
+          <Skeleton className='h-4 w-20' />
+        </div>
+      </div>
+
+      {/* Post Title */}
+      <div className='text-lg mb-4 font-semibold text-gray-100'>
+        <Skeleton className='h-6 w-72' />
+      </div>
+
+      {/* Interaction Bar */}
+      <div className='flex items-center space-x-6 text-sm text-gray-400'>
+        <div className='flex items-center space-x-1'>
+          <Skeleton className='h-4 w-20' />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 interface CommunityPostProps {
@@ -58,25 +79,32 @@ interface CommunityPostProps {
 }
 
 export function CommunityPost(CommunityPostProps: CommunityPostProps) {
-    return (
-      <div className='border text-gray-200 p-4 rounded-md mx-auto mb-4'>
-        <div className='flex items-center space-x-2 mb-2'>
-          <div className='flex items-center space-x-1'>
-            <span className='text-sm text-gray-400'>{CommunityPostProps.author}</span>
-            <span className='text-sm text-gray-400'>•</span>
-            <span className='text-sm text-gray-400'>{CommunityPostProps.createdAt ? getTimeBetweenDates(CommunityPostProps.createdAt, new Date()) : 'Unknown date'}</span>
-          </div>
-        </div>
-  
-        {/* Post Title */}
-        <div className='text-lg mb-4 font-semibold text-gray-100'>{CommunityPostProps.title}</div>
-  
-        {/* Interaction Bar */}
-        <div className='flex items-center space-x-6 text-sm text-gray-400'>
-          <div className='flex items-center space-x-1'>
-            <ButtonGroup id={CommunityPostProps.id}  title={CommunityPostProps.title} content={CommunityPostProps.title} score={CommunityPostProps.score} author={CommunityPostProps.author} createdAt={CommunityPostProps.createdAt}/>
-          </div>
+  return (
+    <div className='border text-gray-200 p-4 rounded-md mx-auto mb-4'>
+      <div className='flex items-center space-x-2 mb-2'>
+        <div className='flex items-center space-x-1'>
+          <span className='text-sm text-gray-400'>{CommunityPostProps.author}</span>
+          <span className='text-sm text-gray-400'>•</span>
+          <CommunityPostDate createdAt={CommunityPostProps.createdAt} />
         </div>
       </div>
-    );
-  }
+
+      {/* Post Title */}
+      <div className='text-lg mb-4 font-semibold text-gray-100'>{CommunityPostProps.title}</div>
+
+      {/* Interaction Bar */}
+      <div className='flex items-center space-x-6 text-sm text-gray-400'>
+        <div className='flex items-center space-x-1'>
+          <ButtonGroup
+            id={CommunityPostProps.id}
+            title={CommunityPostProps.title}
+            content={CommunityPostProps.title}
+            score={CommunityPostProps.score}
+            author={CommunityPostProps.author}
+            createdAt={CommunityPostProps.createdAt}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
