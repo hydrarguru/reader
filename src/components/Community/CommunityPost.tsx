79eba74cc3bcccea94b0220b0bb6c8c4d3460a1b
hydrarguru <henrik@engqvist.org>
2024-12-12 13:31:17 +0100

    refactor: post date is more clear.

--- src/components/Community/CommunityPost.tsx
+++ src/components/Community/CommunityPost.tsx
@@ -29,7 +29,22 @@ export function CommunityPostSkeleton() {
 
 function getTimeBetweenDates(date1: Date, date2: Date): string {
   const diff = Math.abs(date1.getTime() - date2.getTime());
-  return `${Math.floor(diff / (1000 * 60 * 60 * 24))} days ago`;
+
+  const seconds = Math.floor(diff / 1000);
+  const minutes = Math.floor(seconds / 60);
+  const hours = Math.floor(minutes / 60);
+  const days = Math.floor(hours / 24);
+
+  if (seconds < 60) {
+    return `${seconds} seconds ago`;
+  } else if (minutes < 60) {
+    return `${minutes} minutes ago`;
+  } else if (hours < 24) {
+    return `${hours} hours ago`;
+  } else {
+    return `${days} days ago`;
+  }
+
 }
 
 interface CommunityPostProps {
