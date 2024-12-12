67017b4d027898d2b491f1c2511cdc6303b1e26a
hydrarguru <henrik@engqvist.org>
2024-12-12 21:53:40 +0100

    refactor: split up NavBar component into smaller components.

--- src/components/Navigation/NavBar.tsx
+++ src/components/Navigation/NavBar.tsx
@@ -1,19 +1,8 @@
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuGroup,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu';
-import { User, Settings, LogOut, LogIn, UserPlus } from 'lucide-react';
-import { ThemeToggleButton } from '../Theme/ThemeToggleButton';
-//import { NavBarSearch } from './NavBarSearch';
-import { Button } from '../ui/button';
+import { NavLink } from 'react-router';
 import { Community } from '@/types/CommunityType';
 import { useEffect, useState } from 'react';
-import { NavLink } from 'react-router';
+import { MobileDrawer } from './MobileDrawer';
+import { DesktopMenu } from './DesktopMenu';
 
 interface NavBarProps {
   communities: Community[];
@@ -21,9 +10,11 @@ interface NavBarProps {
   isUserLoggedIn: boolean;
 }
 
+
+
 //TODO: fix nav width when width is 600px or lower.
 export function NavBar(NavBarProps: NavBarProps) {
-  const [userLoginState, setUserLoginState] = useState<boolean | null>(null);
+  const [userLoginState, setUserLoginState] = useState<boolean>(false);
 
   useEffect(() => {
     setUserLoginState(NavBarProps.isUserLoggedIn);
@@ -35,7 +26,7 @@ export function NavBar(NavBarProps: NavBarProps) {
         <div className='flex items-center space-x-2'>
           <NavLink to='/' className='flex items-center space-x-2' end>
             <span
-              className='text-violet-600 dark:text-violet-600 hover:text-violet-800 dark:hover:text-violet-800 drop-shadow-sm text-3xl font-extrabold hover:cursor-pointer overflow-hidden transition-all ease-in-out duration-200'
+              className='text-violet-600 dark:text-violet-600 hover:text-violet-800 dark:hover:text-violet-800 drop-shadow-sm text-3xl font-bold hover:cursor-pointer overflow-hidden transition-all ease-in-out duration-200'
               onClick={() => NavBarProps.onCommunityChange(null)}
             >
               Reader
@@ -43,47 +34,8 @@ export function NavBar(NavBarProps: NavBarProps) {
           </NavLink>
         </div>
         <div className='flex pr-4 space-x-4'>
-          <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-              <Button variant='outline' size='icon'>
-                <User strokeWidth={1.5} className='text-violet-600 dark:text-violet-600 hover:text-violet-800 dark:hover:text-violet-800' />
-              </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent className='w-56' align='start'>
-              <DropdownMenuLabel>Account Settings</DropdownMenuLabel>
-              <DropdownMenuSeparator />
-              {userLoginState === true ? (
-                <>
-                  <DropdownMenuGroup>
-                    <DropdownMenuItem className='gap-2'>
-                      <User size={24} strokeWidth={1.5} className='text-black dark:text-white' />
-                      <span>Profile</span>
-                    </DropdownMenuItem>
-                    <DropdownMenuItem className='gap-2'>
-                      <Settings size={24} strokeWidth={1.5} className='text-black dark:text-white' />
-                      <span>Settings</span>
-                    </DropdownMenuItem>
-                    <DropdownMenuItem className='gap-2'>
-                      <LogOut size={24} strokeWidth={1.5} className='text-black dark:text-white' />
-                      <span>Logout</span>
-                    </DropdownMenuItem>
-                  </DropdownMenuGroup>
-                </>
-              ) : (
-                <>
-                  <DropdownMenuItem className='gap-2'>
-                    <LogIn size={24} strokeWidth={1.5} className='text-black dark:text-white' />
-                    <span>Sign in</span>
-                  </DropdownMenuItem>
-                  <DropdownMenuItem className='gap-2'>
-                    <UserPlus size={24} strokeWidth={1.5} className='text-black dark:text-white' />
-                    <span>Sign up</span>
-                  </DropdownMenuItem>
-                </>
-              )}
-            </DropdownMenuContent>
-            <ThemeToggleButton />
-          </DropdownMenu>
+          <MobileDrawer />
+          <DesktopMenu loginState={userLoginState} />
         </div>
       </ul>
     </nav>
