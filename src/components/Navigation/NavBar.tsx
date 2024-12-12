b8a67bd5e78bd903ef7d895e31d4303be188d361
hydrarguru <henrik@engqvist.org>
2024-12-12 18:41:58 +0100

    refactor: Icon styling.

--- src/components/Navigation/NavBar.tsx
+++ src/components/Navigation/NavBar.tsx
@@ -2,27 +2,28 @@ import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
+  DropdownMenuGroup,
   DropdownMenuLabel,
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
-import { User } from 'lucide-react';
+import { User, Settings, LogOut, LogIn, UserPlus } from 'lucide-react';
 import { ThemeToggleButton } from '../Theme/ThemeToggleButton';
 //import { NavBarSearch } from './NavBarSearch';
 import { Button } from '../ui/button';
 import { Community } from '@/types/CommunityType';
 import { useEffect, useState } from 'react';
-import { NavLink } from "react-router";
+import { NavLink } from 'react-router';
 
 interface NavBarProps {
   communities: Community[];
   onCommunityChange: (community: Community | null) => void;
-	isUserLoggedIn: boolean;
+  isUserLoggedIn: boolean;
 }
 
 //TODO: fix nav width when width is 600px or lower.
 export function NavBar(NavBarProps: NavBarProps) {
-	const [userLoginState, setUserLoginState] = useState<boolean | null>(null);
+  const [userLoginState, setUserLoginState] = useState<boolean | null>(null);
 
   useEffect(() => {
     setUserLoginState(NavBarProps.isUserLoggedIn);
@@ -33,35 +34,57 @@ export function NavBar(NavBarProps: NavBarProps) {
       <ul className='flex justify-between'>
         <div className='flex items-center space-x-2'>
           <NavLink to='/' className='flex items-center space-x-2' end>
-            <span className='text-violet-600 dark:text-violet-600 hover:text-violet-800 dark:hover:text-violet-800 drop-shadow-sm text-3xl font-extrabold hover:cursor-pointer overflow-hidden transition-all ease-in-out duration-200' onClick={() => NavBarProps.onCommunityChange(null)}>Reader</span>
+            <span
+              className='text-violet-600 dark:text-violet-600 hover:text-violet-800 dark:hover:text-violet-800 drop-shadow-sm text-3xl font-extrabold hover:cursor-pointer overflow-hidden transition-all ease-in-out duration-200'
+              onClick={() => NavBarProps.onCommunityChange(null)}
+            >
+              Reader
+            </span>
           </NavLink>
         </div>
-				<div className='flex pr-4 space-x-4'>
-        <DropdownMenu>
-				<DropdownMenuTrigger asChild>
-					<Button variant='outline' size='icon'>
-						<User className='text-violet-600 dark:text-violet-600 hover:text-violet-800 dark:hover:text-violet-800'/>
-					</Button>
-				</DropdownMenuTrigger>
-          <DropdownMenuContent align={'center'}>
-            <DropdownMenuLabel>My Account</DropdownMenuLabel>
-            <DropdownMenuSeparator />
-             {userLoginState === true ? (
-							<>
-              <DropdownMenuItem>Profile</DropdownMenuItem>
-							<DropdownMenuItem>Settings</DropdownMenuItem>
-							<DropdownMenuItem>Logout</DropdownMenuItem>
-							</>
-            ) : (
-							<>
-              <DropdownMenuItem>Sign in</DropdownMenuItem>
-							<DropdownMenuItem>Sign up</DropdownMenuItem>
-							</>
-             )}
-          </DropdownMenuContent>
-          <ThemeToggleButton />
-        </DropdownMenu>
-				</div>
+        <div className='flex pr-4 space-x-4'>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant='outline' size='icon'>
+                <User strokeWidth={1.5} className='text-violet-600 dark:text-violet-600 hover:text-violet-800 dark:hover:text-violet-800' />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className='w-56' align='start'>
+              <DropdownMenuLabel>Account Settings</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              {userLoginState === true ? (
+                <>
+                  <DropdownMenuGroup>
+                    <DropdownMenuItem className='gap-2'>
+                      <User size={24} strokeWidth={1.5} className='text-black dark:text-white' />
+                      <span>Profile</span>
+                    </DropdownMenuItem>
+                    <DropdownMenuItem className='gap-2'>
+                      <Settings size={24} strokeWidth={1.5} className='text-black dark:text-white' />
+                      <span>Settings</span>
+                    </DropdownMenuItem>
+                    <DropdownMenuItem className='gap-2'>
+                      <LogOut size={24} strokeWidth={1.5} className='text-black dark:text-white' />
+                      <span>Logout</span>
+                    </DropdownMenuItem>
+                  </DropdownMenuGroup>
+                </>
+              ) : (
+                <>
+                  <DropdownMenuItem className='gap-2'>
+                    <LogIn size={24} strokeWidth={1.5} className='text-black dark:text-white' />
+                    <span>Sign in</span>
+                  </DropdownMenuItem>
+                  <DropdownMenuItem className='gap-2'>
+                    <UserPlus size={24} strokeWidth={1.5} className='text-black dark:text-white' />
+                    <span>Sign up</span>
+                  </DropdownMenuItem>
+                </>
+              )}
+            </DropdownMenuContent>
+            <ThemeToggleButton />
+          </DropdownMenu>
+        </div>
       </ul>
     </nav>
   );
--- src/components/Theme/ThemeToggleButton.tsx
+++ src/components/Theme/ThemeToggleButton.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Moon, Sun } from 'lucide-react';
+import { Moon, Sun, SunMoon } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 import { useTheme } from '@/components/Theme/ThemeProvider';
 
@@ -16,7 +16,7 @@ export function ThemeToggleButton() {
   return (
     <Button variant='outline' size='icon' onClick={() => handleThemeChange()}>
       <Sun className='text-violet-600 dark:text-violet-600 hover:text-violet-800 dark:hover:text-violet-800 h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0' />
-      <Moon className='text-violet-600 dark:text-violet-600 hover:text-violet-800 dark:hover:text-violet-800 absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100' />
+      <SunMoon className='text-violet-600 dark:text-violet-600 hover:text-violet-800 dark:hover:text-violet-800 absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100' />
       <span className='sr-only'>Toggle theme</span>
     </Button>
   );
