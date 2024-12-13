9b6d89e024ce6451f090fe6d1d082ddd14228dfe
hydrarguru <henrik@engqvist.org>
2024-12-13 01:11:16 +0100

    refactor: styling tweaks and tooltip.
    - Tweaked some styling to fit the current theme(light & dark).
    - Added Tooltip component to ThemeToggleButton component.

--- src/components/Theme/ThemeToggleButton.tsx
+++ src/components/Theme/ThemeToggleButton.tsx
@@ -1,13 +1,10 @@
 import { useState } from 'react';
-import { Moon, Sun, SunMoon } from 'lucide-react';
+import { Sun, SunMoon } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 import { useTheme } from '@/components/Theme/ThemeProvider';
+import { ComponentToolTip } from '../Tooltip';
 
-interface ThemeToggleButtonProps {
-  className?: string;
-}
-
-export function ThemeToggleButton({ className }: ThemeToggleButtonProps) {
+export function ThemeToggleButton() {
   const { setTheme } = useTheme();
   const [currentTheme, setCurrentTheme] = useState('light');
 
@@ -18,10 +15,17 @@ export function ThemeToggleButton({ className }: ThemeToggleButtonProps) {
   }
 
   return (
-    <Button className={className} variant='outline' size='icon' onClick={() => handleThemeChange()}>
-      <Sun className='text-violet-600 dark:text-violet-600 hover:text-violet-800 dark:hover:text-violet-800 h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0' />
-      <SunMoon className='text-violet-600 dark:text-violet-600 hover:text-violet-800 dark:hover:text-violet-800 absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100' />
-      <span className='sr-only'>Toggle theme</span>
-    </Button>
+    <ComponentToolTip toolTip='Toggle Theme' align='center' delayDur={500}>
+      <Button
+        className='text-violet-600 border-violet-600 hover:bg-violet-800 hover:border-zinc-700 hover:text-white dark:bg-zinc-950 dark:hover:border-lime-500 dark:hover:bg-zinc-950 dark:hover:text-lime-500'
+        variant='outline'
+        size='icon'
+        onClick={() => handleThemeChange()}
+      >
+        <Sun className='h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0' />
+        <SunMoon className='absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100' />
+        <span className='sr-only'>Toggle theme</span>
+      </Button>
+    </ComponentToolTip>
   );
 }
