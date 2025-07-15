08cc7f251f637c236530d52f6f937a35adfa1480
hydrarguru <henrik@engqvist.org>
2025-07-15 23:53:50 +0200

    feat: basic home page.

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,6 +1,5 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
-
+import { ThemeProvider } from "../components/theme/theme-provider";
 export function meta({}: Route.MetaArgs) {
   return [
     { title: "New React Router App" },
@@ -9,5 +8,22 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <main className="flex min-h-screen flex-col gap-4 p-4">
+        <h1 className="text-3xl font-bold">Reader Home Page</h1>
+        <p>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five centuries, but also the leap into electronic typesetting,
+          remaining essentially unchanged. It was popularised in the 1960s with
+          the release of Letraset sheets containing Lorem Ipsum passages, and
+          more recently with desktop publishing software like Aldus PageMaker
+          including versions of Lorem Ipsum.
+        </p>
+      </main>
+    </ThemeProvider>
+  );
 }
